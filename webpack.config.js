dde9a63a158159e6c9d1e341aa4e2cb2affb1a9d
daewoongkim <insidedw@gmail.com>
2024-06-13 09:39:18 +0900

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        background: './background.js',
+        content: './index.js',
+        popup: './popup.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    mode: 'development',
+    devtool: 'source-map'
+};
